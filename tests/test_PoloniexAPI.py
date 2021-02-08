192b37e72a1a7bcce23f28dc1303dedaedcf1209
Inigo <igponce@gmail.com>
2021-02-09 00:05:40 +0100

    Test passes

--- tests/test_PoloniexAPI.py
+++ tests/test_PoloniexAPI.py
@@ -43,6 +43,6 @@ def api_rate_limit(n, start):
 # Test rate limiter
 def test_rate_limiter():
     start = time.time()
-    for i in xrange(20):
+    for i in range(20):
         thread1 = threading.Thread(target=api_rate_limit, args=(i, start))
         thread1.start()
